408091efcd3cbba3e48dbf319b9e7eec18b34920
EddieJorden <eddie@eddiejorden.tech>
2021-09-04 13:35:44 -0700

    got some things ==ej==

--- src/components/IntroMessage/IntroMessage.jsx
+++ src/components/IntroMessage/IntroMessage.jsx
@@ -1,23 +1,46 @@
 import React, { useEffect, useState } from 'react';
 
 const IntroMessage = () => {
-    const url = 'https://randomuser.me/api/'
+
+    //generic click event
     const [count, setCount] = useState(0);
+
+    // person is assigned data.results[0]
     const [person, setPerson] = useState(null);
 
+    // assigned to img from api respones?
+    const [img, setImg] = useState(null);
 
-    useEffect(async() => {
-        const response = await fetch(url)
+    // loading ???
+    const [loading, setLoading] = useState(true)
+
+    // useEffect() just so that it auto refreshes on value change of count when a user click's button it updates count
+    useEffect(async () => {
+
+        // response assigned to the awaiting of fetch(calling api url)
+        const response = await fetch('https://randomuser.me/api/')
+            
+
+        // data assigned the awaiting of response.json() which must be a react or javascript method that can call json()??
         const data = await response.json()
-        const item = data.results[0]
+
+        // item must be getting assigned the data being assigned as object name? possible an array sine there is in index?
+        const item = data.results[0];
         setPerson(item)
+        
+        setLoading(false)
+
     }, [count])
+
+        
 
     return (
         <div>
             <p>you have clicked {count} times</p>
             <button onClick={() => setCount(count + 1)}>Click me</button>
             {person && <div>{person.name.first}</div>}
+            {person && <div>{person.name.last}</div>}
+            {loading ? <div>...loading</div> : <img src={person.picture.large}/>}
         </div>
     )
 }
